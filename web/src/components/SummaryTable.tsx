8e210fa63966a337f3cbcae115643f22d9633748
rhuanbello <rhuambello@gmail.com>
2023-01-19 17:17:57 -0300

    [aula 02] Web ✅

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,45 @@
+import { SummaryDatesProps } from "../types/summaryDates";
+import { generateDatesFromYearBeginning } from "../utils/genareteDatesFromYearBeginning"
+import { HabitDay } from "./HabitDay"
+
+const weekDays = [
+  'D', 'S', 'T', 'Q', 'Q', 'S', 'S'
+]
+
+let summaryDates: SummaryDatesProps[] = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks * 7 days = 126 days
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
+if (amountOfDaysToFill > 0) {
+  summaryDates = [
+    ...summaryDates,
+    ...Array.from({ length: amountOfDaysToFill }, () => false) as false[]
+  ]
+}
+
+export const SummaryTable = () => {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((day, i) => (
+          <div
+            key={i}
+            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+          >
+            {day}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date, i) => (
+          <HabitDay
+            key={i}
+            reachedHabitDate={Boolean(date)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ web/src/types/summaryDates.ts
@@ -0,0 +1,1 @@
+export type SummaryDatesProps = Date | false;
