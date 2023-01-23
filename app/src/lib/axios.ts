42fb3445c3e868440b8d511e16fd786a832274c9
rhuanbello <rhuambello@gmail.com>
2023-01-23 13:18:20 -0300

    [chore] Server BaseURL

--- app/src/lib/axios.ts
+++ app/src/lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    "mysql://qys90fy1623lx635dwr7:pscale_pw_R8vjh6IOcpWipXxU7ikZMvMYVblQe4136u1Kz3LF6GI@us-east.connect.psdb.cloud/nlw-setup-ignite?sslaccept=strict",
+  baseURL: "https://rocketseat-nlw-setup-server.vercel.app",
 });
